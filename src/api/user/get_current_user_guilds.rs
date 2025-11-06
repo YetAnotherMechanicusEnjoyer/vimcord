6f4afaca3e9f2f51daab558664a9c07f88c5e8c1
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2025-11-06 17:32:38 +0100

    [ADD] src/api/user/get_current_user_guilds.rs

---
+++ src/api/user/get_current_user_guilds.rs
@@ -0,0 +1,22 @@
+use reqwest::Client;
+
+use crate::model::guild::Guild;
+
+pub async fn get_current_user_guilds(client: &Client, token: &str) -> Result<Vec<Guild>, String> {
+    let url = "https://discord.com/api/v10/users/@me/guilds?limit=100";
+    let response = client
+        .get(url)
+        .header("Authorization", token)
+        .send()
+        .await
+        .map_err(|e| format!("API Error: {e}"))?;
+
+    if response.status().is_success() {
+        response
+            .json()
+            .await
+            .map_err(|e| format!("JSON Error: {e}"))
+    } else {
+        Err(format!("API Error: {}", response.status()))
+    }
+}
