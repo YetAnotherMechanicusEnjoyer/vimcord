a8218531e82af90efc9ba1928f52c40a8755f5f1
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2026-03-22 03:16:34 +0100

    [ADD] PartialGuild struct & roles to Guild struct

--- src/api/guild.rs
+++ src/api/guild.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-use crate::api::User;
+use crate::api::{User, channel::Role};
 
 #[derive(Debug, Deserialize, Clone)]
 pub struct GuildMember {
@@ -9,7 +9,14 @@ pub struct GuildMember {
 }
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct PartialGuild {
+    pub id: String,
+    pub name: String,
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct Guild {
     pub id: String,
     pub name: String,
+    pub roles: Vec<Role>,
 }
